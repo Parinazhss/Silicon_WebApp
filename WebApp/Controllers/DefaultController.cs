81ba1b4147113cec8e8b8ee024fd1a8a688ae585
Parinaz <parinazhss87@gmail.com>
2024-04-16 10:27:12 +0200

    Completed Subscribe for newsletter feature.

--- WebApp/Controllers/DefaultController.cs
+++ WebApp/Controllers/DefaultController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebApp.ViewModels;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers;
+
+public class DefaultController(HttpClient httpClient) : Controller
 {
-    public class DefaultController : Controller
+    private readonly HttpClient _httpClient = httpClient;
+
+    public IActionResult Home()
     {
-        public IActionResult Home()
-        {
-            return View();
+       
+        return View();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Subscribe(SubscribeViewModel model)
+    {
+        if (ModelState.IsValid)
+        { 
+            var content =  new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8 , "application/json" );
+            var response = await _httpClient.PostAsync("https://localhost:7055/api/subscribe", content);
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["StatusMessage"] = "You are now subscribed.";
+
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.Conflict) 
+            {
+                TempData["StatusMessage"] = "You are already subscribed.";
+            }
         }
+        else
+        {
+            TempData["StatusMessage"] = "Invalid Email Address";
+        }
+
+        return RedirectToAction("Home", "Default" , "subscribe");
+
     }
 }
+
---
+++ WebApp/ViewModels/SubscribeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels;
+
+public class SubscribeViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email", Prompt ="Your email")]
+    public string Email { get; set; } = null!;
+
+    public bool DailyNewsletter { get; set; }
+
+    public bool AdvertisingUpdates { get; set; }
+
+    public bool WeekinReviews { get; set; }
+
+    public bool EventUpdate { get; set; }
+
+    public bool StartupsWeekly { get; set; }
+
+    public bool Podcaste { get; set; }
+
+}
